d2368e1842468ee96d25fefc8d28a956598e158f
robbcorrea <51095085+RobbCorrea@users.noreply.github.com>
2019-11-26 19:35:19 -0600

    Acceleration succesfully simulated for both players, plus css new start button

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 //main.js
-let canvas, canvasContext;
+//main.js
+//let canvas, canvasContext;
 let myGamePiece;
 let myGamePiece2;
-
+//Sumo Kart Variables
 const FRICCION = 0.94;
-const ACELERADOR = 0.5;
-const REVERSA = 0.2;
+const ACELERACION = 0.5;
+let REVERSA = 0.2;
+//Winner Variable
+//const WINNER = (myGamePiece.speed>myGamePiece2.speed)
 //const sumoAlpha = new sumoClass();
 //const sumoBravo = new sumoClass();
+
+//MUSIC
+
+var battleMusic = new Audio("./sounds/3687-edm-detection-mode-by-kevin-macleod.mp3");
+            battleMusic.preload = 'auto';
+            battleMusic.loop = 'true';
+            battleMusic.load();
+
+var bgMusic = document.getElementById("introTheme");
+
+bgMusic.play();
+
 
 document.getElementById("boton-inicia-juego").onclick = function() {
     
@@ -18,10 +33,10 @@ document.getElementById("boton-inicia-juego").onclick = function() {
     //playBackMusic(1);
 
     startGame();
-    
-    canvas = document.getElementById('canvasJuego');
-    canvasContext = canvas.getContext('2d');
-
+    //loadCrono(); Aquí cargaré mi cronómetro.
+    //canvas = document.getElementById('canvasJuego');
+    //canvasContext = canvas.getContext('2d');
+    playBattleMusic(1);
     //colorRect(0,0, canvas.width,canvas.height, 'black');
     //colorText("Montando el ring...", canvas.width/2, canvas.height/2, 'white');
 
@@ -29,18 +44,29 @@ document.getElementById("boton-inicia-juego").onclick = function() {
 }
 
 
-            
+Window.onload = function() {
+
+}
+
+
+
+const playBattleMusic = (volume) => {
+    const click = battleMusic.cloneNode();
+    click.volume = volume;
+    click.play();
+}
+
             function startGame() {
-                myGamePiece = new component(30,30, "red",  200, 135);
-                myGamePiece2 = new component(30,30, "blue",  240, 135);
+                myGamePiece = new component(40,40, "red",  200, 135);
+                myGamePiece2 = new component(40,40, "blue",  240, 135);
                 myGameArea.start();
             }
             
             var myGameArea = {
                 canvas : document.createElement("canvas"),
                 start : function() {
-                    this.canvas.width = 480;
-                    this.canvas.height = 270;
+                    this.canvas.width = 800;
+                    this.canvas.height = 600;
                     this.context = this.canvas.getContext("2d");
                     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
                     this.frameNo = 0;
@@ -53,14 +79,23 @@ document.getElementById("boton-inicia-juego").onclick = function() {
                     window.addEventListener('keyup', function (e) {
                         myGameArea.keys[e.keyCode] = (e.type == "keydown");
                     })
+                                    playMusic : function playBackMusic(volume){
+                            var click = sound3.cloneNode();
+                            click.volume = volume;
+                            click.play();
+                }
                 },
                 stop : function() {
                     clearInterval(this.interval);
                 },    
                 clear : function() {
                     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-                }
+                },
+
+
             }
+
+
             
             function component(width, height, color, x, y, type) {
             
@@ -70,6 +105,7 @@ document.getElementById("boton-inicia-juego").onclick = function() {
                 this.speed = 0;
                 this.angle = 0;
                 this.moveAngle = 0;
+                this.acelerador = 0;
                 this.x = x;
                 this.y = y;    
                 this.update = function() {
@@ -86,9 +122,13 @@ document.getElementById("boton-inicia-juego").onclick = function() {
                     this.x += this.speed * Math.sin(this.angle);
                     this.y -= this.speed * Math.cos(this.angle);
                 }
-  //codigo de JP para la colision              this.checkCollition = function(enemy) {
 
-            //    }
+                this.crashWith = (item) => {
+                    return (this.x < item.x + item.width) &&
+                    (this.x + this.width > item.x) &&
+                    (this.y < item.y + item.height) &&
+                    (this.y + this.height > item.y);
+                }
             }
             //Funciones de movimiento de minisumos
             function updateGameArea() {
@@ -98,25 +138,42 @@ document.getElementById("boton-inicia-juego").onclick = function() {
                 myGamePiece.speed = 0;
                 if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.moveAngle = -1; }
                 if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.moveAngle = 1; }
-                if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speed= 1.5 + ACELERADOR; }
-                if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speed= -1.5 + REVERSA; }
-                if (myGamePiece.speed>myGamePiece2.speed) {console.log("Red's faster.");}
+                if (myGameArea.keys && myGameArea.keys[38]) {
+                    myGamePiece.speed = 1.5 + myGamePiece.acelerador;
+                    myGamePiece.acelerador += 0.1;
+                    console.log(myGamePiece.speed);
+                } else if (myGameArea.keys && myGameArea.keys[40]) {
+                    myGamePiece.speed = -1.5 - myGamePiece.acelerador;
+                    myGamePiece.acelerador += 0.1;
+                } else {
+                    myGamePiece.acelerador = 0;
+                }
+    
+                //if (myGamePiece.speed>myGamePiece2.speed) {console.log("Red's faster.");}
                 myGamePiece.newPos();
                 myGamePiece.update();
+                if (myGamePiece.crashWith(myGamePiece2) && (myGamePiece.speed>myGamePiece2.speed)) {console.log(`Red Gana!`);}
 
                 //Jugador 2
                 myGamePiece2.moveAngle = 0;
                 myGamePiece2.speed = 0;
                 if (myGameArea.keys && myGameArea.keys[65]) {myGamePiece2.moveAngle = -1; }
-                if (myGameArea.keys && myGameArea.keys[68]) {myGamePiece2.moveAngle = 1; }
-                if (myGameArea.keys && myGameArea.keys[87]) {myGamePiece2.speed= 1.5 + ACELERADOR; }
-                if (myGameArea.keys && myGameArea.keys[83]) {myGamePiece2.speed= -1.5 + REVERSA; }
-                if (myGamePiece2.speed>myGamePiece.speed) {console.log("Blue's faster.");}
+                if (myGameArea.keys && myGameArea.keys[68]) {myGamePiece2.moveAngle = 1; } 
+                if (myGameArea.keys && myGameArea.keys[87]) {
+                    myGamePiece2.speed = 1.5 + myGamePiece2.acelerador;
+                    myGamePiece2.acelerador += 0.1;
+                    console.log(myGamePiece2.speed);
+                } else if (myGameArea.keys && myGameArea.keys[83]) {
+                    myGamePiece2.speed = -1.5 - myGamePiece2.acelerador;
+                    myGamePiece2.acelerador += 0.1;
+                } else {
+                    myGamePiece2.acelerador = 0
+                }
+                //if (myGamePiece2.speed>myGamePiece.speed) {console.log("Blue's faster.");}
                 myGamePiece2.newPos();
                 myGamePiece2.update();
-
-
-               
+                if (myGamePiece2.crashWith(myGamePiece) && (myGamePiece2.speed>myGamePiece.speed || -myGamePiece2.speed > -myGamePiece.speed)) {console.log(`Blue Gana! ${myGamePiece2.acelerador}`);} //Aquí es si ganó en reversa.
+             //   if (gameOver()) {console.log(`¿Te gustaría comprar estas piezas?`)}
 
                 //Para ganar. Revisar cómo hacer la aceleración en tutorial.
                 //Cuando veamos cómo hacer la aceleración, revisar cómo hacer la colisión.
@@ -126,3 +183,49 @@ document.getElementById("boton-inicia-juego").onclick = function() {
                 //también con base en la resta se puede ejecutar una función oncollision
                 //que muestre una de varias posibles animaciones.
             }
+/*
+            const gameOver = (winner) => {
+
+               winner =  myGamePiece.crashWith(myGamePiece2) || myGamePiece2.crashWith(myGamePiece) ? gameOver() : console.log(frames);
+               return winner; 
+            
+            }
+*/
+            //CLASS LOAD IMAGES FOR MINISUMOS AND CARS
+           /* function beginLoadingImage(imgVar, fileName) {
+                imgVar.onload = countLoadedImagesAndLaunchIfReady;
+                imgVar.src = "images/"+fileName;
+            }
+
+            function loadImageForTrackCode(trackCode, fileName) {
+                trackPics[trackCode] = document.createElement("img");
+                beginLoadingImage(trackPics[trackCode], fileName);
+            }
+
+            function loadImages() {
+                var imageList = [
+
+                    {varName: redPic, theFile: "red.png"},
+                    {varName: bluePic, theFile: "blue.png"},
+                    {varName: bluePic, theFile: "redUpGrade.png"},
+                    {varName: bluePic, theFile: "blueUpGrade.png"},
+            
+                    {trackType: TRACK_ROAD, theFile: "track_road.png"},
+                    {trackType: TRACK_WALL, theFile: "track_wall.png"},
+                    {trackType: TRACK_GOAL, theFile: "track_goal.png"},
+                    
+                ];
+            
+                picsToLoad = imageList.length;
+            
+                for(var i=0;i<imageList.length;i++) {
+                    if(imageList[i].varName != undefined) {
+                        beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+                    } else {
+                        loadImageForTrackCode(imageList[i].trackType, imageList[i].theFile);
+                    }
+                }
+            } 
+            */
+
+            
